d9de685bf72c72e7923bc3153c6c73cf958396b7
Mitzio Riquelme <mitzioriquelmes@gmail.com>
2025-12-28 20:15:12 +0000

    Actualización lista desplegable instituciones

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,21 +1,21 @@
 import Dashboard from './pages/Dashboard';
 import ICSStructure from './pages/ICSStructure';
-import IncidentDetail from './pages/IncidentDetail';
 import IncidentMap from './pages/IncidentMap';
 import Incidents from './pages/Incidents';
 import Reports from './pages/Reports';
 import Resources from './pages/Resources';
+import IncidentDetail from './pages/IncidentDetail';
 import __Layout from './Layout.jsx';
 
 
 export const PAGES = {
     "Dashboard": Dashboard,
     "ICSStructure": ICSStructure,
-    "IncidentDetail": IncidentDetail,
     "IncidentMap": IncidentMap,
     "Incidents": Incidents,
     "Reports": Reports,
     "Resources": Resources,
+    "IncidentDetail": IncidentDetail,
 }
 
 export const pagesConfig = {
